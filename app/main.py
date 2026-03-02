9c6c09fe18692dfb8d9af4b786f574e4a84467de
RdxPrasad <prasadawareofficial@gmail.com>
2026-03-03 02:00:38 +0530

    Initial backend setup with project structure and database configuration

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.database import engine , Base
+
+app = FastAPI() 
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message" : "Event & Certification Automation Backend Running 🎉"}
